4858c3b41ab3ae6da82b688f3db640a907d24dd0
Jacksonm99 <jacksonmanoel1999@gmail.com>
2021-09-22 22:06:14 -0300

    Cria Classes Cliente, Endereco e Validacoes

---
+++ AgenciaBancaria.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("Aluisio de azevedo", "50100090","Santo Amaro", "Recife", "Pernambuco");
+                Cliente cliente = new Cliente("Jackson", "123456", "9491845",endereco);
+                Console.WriteLine(cliente.Nome);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ AgenciaBancaria.Dominio/Endereco.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+   public class Endereco
+    {
+        public Endereco(string rua, string cep,string bairro, string cidade, string estado)
+        {
+            Rua = rua.ValidaStringVazia();
+            CEP = cep.ValidaStringVazia();
+            Bairro = bairro.ValidaStringVazia();
+            Cidade = cidade.ValidaStringVazia();
+            Estado = estado.ValidaStringVazia();
+        }
+        public string Rua { get; private set; }
+        public string CEP { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+
+    }
+}
